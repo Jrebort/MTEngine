a13b132da91bb77f8b416696d1c5b998315135e9
Jrebort <39721924+Jrebort@users.noreply.github.com>
2023-08-16 14:44:19 +0800

    Upload windows event callback

--- MTEngine/src/MTEngine/Application.cpp
+++ MTEngine/src/MTEngine/Application.cpp
@@ -7,8 +7,12 @@
 #include <GLFW/glfw3.h>
 
 namespace MTEngine {
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application() {
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application() {
@@ -21,5 +25,17 @@ namespace MTEngine {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	void Application::OnEvent(Event & e) {
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+			
+		MT_CORE_TRACE("{0}", e);
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e) {
+		m_Running = false;
+		return true;
 	}
 }
--- MTEngine/src/MTEngine/Application.h
+++ MTEngine/src/MTEngine/Application.h
@@ -3,6 +3,7 @@
 #include "Core.h"
 #include "Events/Event.h"
 #include "Window.h"
+#include "Events/ApplicationEvent.h"
 
 namespace MTEngine {
 
@@ -13,7 +14,10 @@ namespace MTEngine {
 	 	virtual ~Application();
 
 		void run();
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
