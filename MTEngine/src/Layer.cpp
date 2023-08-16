b47e11c84e53e09570ba34af7325f1e55fcb85e6
Jrebort <39721924+Jrebort@users.noreply.github.com>
2023-08-16 16:17:10 +0800

    add Layer and LayerStack

---
+++ MTEngine/src/Layer.cpp
@@ -0,0 +1,16 @@
+#include "mtpch.h"
+#include "Layer.h"
+
+namespace MTEngine {
+
+	Layer::Layer(const std::string& debugName)
+		: m_DebugName(debugName)
+	{
+	}
+
+	Layer::~Layer()
+	{
+
+	}
+
+}
---
+++ MTEngine/src/Layer.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "MTEngine/Core.h"
+#include "MTEngine/Events/Event.h"
+
+namespace MTEngine {
+
+	class MTENGINE_API Layer
+	{
+	public:
+		Layer(const std::string& name = "Layer");
+		virtual ~Layer();
+
+		virtual void OnAttach() {}
+		virtual void OnDetach() {}
+		virtual void OnUpdate() {}
+		virtual void OnEvent(Event& event) {}
+
+		inline const std::string& GetName() const { return m_DebugName; }
+	protected:
+		std::string m_DebugName;
+	};
+
+}
